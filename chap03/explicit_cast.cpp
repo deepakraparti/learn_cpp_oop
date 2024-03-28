e6e12abd6fa09e68c4f5e269330b8f2038dd34c1
Deepak Raparti <deepakraparti@gmail.com>
2024-03-28 23:58:14 +0530

    Explicit casting in c++ example

---
+++ chap03/explicit_cast.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+  int intvar = 25;
+  float floatvar = 35.87;
+
+  cout << "intvar = " << intvar << endl;
+  cout << "floatvar = " << floatvar << endl;
+  cout << "float(intvar) = " << float(intvar) << endl;
+  cout << "int(floatvar) = " << int(floatvar) << endl;
+
+  return 0;
+}
