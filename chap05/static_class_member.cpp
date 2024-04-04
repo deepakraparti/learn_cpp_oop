1bfa95a9651e0b05c1db50d20159a23ef297050f
Deepak Raparti <deepakraparti@gmail.com>
2024-04-04 23:48:46 +0530

    Example to demonstrate the use of static qualifier for the member variable

---
+++ chap05/static_class_member.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+class item
+{
+    static int count;
+    int number;
+  public:
+    void getdata(int a)
+    {
+      number = a;
+      count++;
+    }
+
+    void getcount(void)
+    {
+      cout << "Count: " << count << "\n";
+    }
+};
+
+int item::count;
+
+int main()
+{
+  item a, b, c;  // count gets initialized to zero here
+  a.getcount();
+  b.getcount();
+  c.getcount();
+
+  a.getdata(100);
+  b.getdata(200);
+  c.getdata(300);
+
+  cout << "After reading data\n";
+
+  a.getcount();
+  b.getcount();
+  c.getcount();
+
+  return 0;
+}
