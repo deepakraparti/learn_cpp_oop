86efae83397217780c0dcbcb6a93d89228ec63d0
Deepak Raparti <deepakraparti@gmail.com>
2024-04-05 23:36:59 +0530

    This commit includes the examples which demonstrates the use of the friend function

---
+++ friend_function_two_classes.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class ABC;	// forward declaration
+class XYZ
+{
+    int x;
+  public:
+    void setvalue(int i) {x = i;}
+    friend void max(XYZ, ABC);
+};
+
+class ABC
+{
+    int a;
+  public:
+    void setvalue(int i) { a = i;}
+    friend void max(XYZ, ABC);
+};
+
+void max(XYZ m, ABC n) // Definition of friend function
+{
+  if (m.x > n.a)
+    cout << m.x;
+  else
+    cout << n.a;
+}
+
+int main()
+{
+  ABC abc;
+  abc.setvalue(10);
+  XYZ xyz;
+  xyz.setvalue(20);
+  max(xyz, abc);
+
+  return 0;
+}
---
+++ swapping_private_data_of_classes.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class class_2;
+
+class class_1
+{
+    int value1;
+  public:
+    void indata(int a) { value1 = a ;}
+    void display(void) { cout << value1 << "\n"; }
+    friend void exchange(class_1 &, class_2 &);
+};
+
+class class_2
+{
+    int value2;
+  public:
+    void indata(int a) { value2 = a ;}
+    void display(void) { cout << value2 << "\n"; }
+    friend void exchange(class_1 &, class_2 &);
+};
+
+void exchange(class_1 &x, class_2 &y)
+{
+  int temp = x.value1;
+  x.value1= y.value2;
+  y.value2 = temp;
+}
+
+int main()
+{
+  class_1 c1;
+  class_2 c2;
+
+  c1.indata(100);
+  c2.indata(200);
+
+  cout << "Values before exchage:\n";
+  c1.display();
+  c2.display();
+ 
+  exchange(c1, c2);
+  
+  cout << "Values after exchange:\n";
+  c1.display();
+  c2.display();
+
+  return 0;
+}
