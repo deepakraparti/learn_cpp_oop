389970b5b6aa69ecfa8aad9e7643aea003b6ed0d
Deepak Raparti <deepakraparti@gmail.com>
2024-10-10 00:07:11 +0530

    Example of user-defined manipulator

---
+++ chap10/user_defined_manipulator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// User-defined manipulators
+ostream & currency(ostream & output)
+{
+  output << "Rs";
+  return output;
+}
+
+ostream & form(ostream & output)
+{
+  output.setf(ios::showpos);
+  output.setf(ios::showpoint);
+  output.fill('*');
+  output.precision(2);
+  output << setiosflags(ios::fixed)
+         << setw(10);
+  return output;
+}
+
+int main()
+{
+  cout << currency << form << 7864.5;
+
+  return 0;
+}
