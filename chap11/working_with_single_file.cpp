76ec11b57cfed62fde0f686b636dbdfcaf9ef994
Deepak Raparti <deepakraparti@gmail.com>
2024-10-14 23:33:17 +0530

    Simple program demonstrating the use of ifstream and ofstream on single file

---
+++ chap11/working_with_single_file.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  ofstream outf("ITEM");
+
+  cout << "Enter item name: ";
+  char name[30];
+  cin >> name;
+
+  outf << name << "\n";
+
+  cout << "Enter item cost:";
+  float cost;
+  cin >> cost;
+
+  outf << cost << "\n";
+
+  outf.close();
+
+  ifstream inf("ITEM");
+
+  inf >> name;
+  inf >> cost;
+  cout << "\n";
+  cout << "Item Name: " << name << "\n";
+  cout << "Item Cost: " << cost << "\n";
+
+  inf.close();
+
+  return 0;
+}
