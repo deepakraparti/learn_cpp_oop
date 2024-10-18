7c68be196167b3e356feef247b51d7f125d7ba8c
Deepak Raparti <deepakraparti@gmail.com>
2024-10-18 20:31:53 +0530

    class template with multiple params and default data types

---
+++ chap12/class_template_with_multiple_params.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T1, class T2>
+class Test
+{
+    T1 a;
+    T2 b;
+
+  public:
+    Test(T1 x, T2 y)
+    {
+      a = x;
+      b = y;
+    }
+
+    void show()
+    {
+      cout << a << " and " << b << "\n";
+    }
+};
+
+int main()
+{
+  cout << "Instantiating the class template as test1 with float and int data types..\ntest1: ";\
+
+  Test <float, int> test1(1.23, 123);
+  test1.show();
+
+  cout << "Instantiating the class template as test2 with int and char data types..\ntest2: ";\
+  Test <int, char> test2(100, 'W');
+  test2.show();
+
+  return 0;
+}
