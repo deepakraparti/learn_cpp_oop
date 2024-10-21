cba7d4b391773ed7905c1ca4689a410b099eb750
Deepak Raparti <deepakraparti@gmail.com>
2024-10-21 22:49:28 +0530

    This program demonstrates the use of vector container and its member functions alongwith some algorithms

---
+++ chap14/using_vectors.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void display(vector<int> &v)
+{
+  for (int i = 0; i < v.size(); i++)
+  {
+    cout << v[i] << " ";
+  }
+
+  cout << "\n";
+}
+
+int main()
+{
+  vector<int> v;
+  cout << "Initial size = " << v.size() << " capacity = " << v.capacity() << endl;
+
+  int x;
+  cout << "Enter five integer values: ";
+  for (int i = 0; i < 5; i++)
+  {
+    cin >> x;
+    v.push_back(x);
+  }
+
+  cout << "Size after adding 5 values= " << v.size() << " capacity = " << v.capacity() << endl;
+
+  cout << "Display the contents: " << endl;
+  display(v);
+
+  // Add one more value
+  v.push_back(6.6);  // float value trucated to int
+
+  cout << "Size = " << v.size() << " capacity = " << v.capacity() << endl;
+  cout << "Contents now: " << endl;
+  display(v);
+
+  // Inserting elements
+  vector<int>::iterator itr = v.begin(); //iterator
+  itr = itr + 3;
+  v.insert(itr, 1, 9);
+
+  cout << "Contents after inserting: " << endl;
+  display(v);
+
+  // Removing 4th and 5th elements
+  v.erase(v.begin() + 3, v.begin() + 5);
+
+  cout << "Contents after deletion: " << endl;
+  display(v);
+
+  // Reverse the vector 
+  reverse(v.begin(), v.end());
+
+  cout << "Contents of the reversed vector: " << endl;
+  display(v);
+
+  // Sort the vector
+  sort(v.begin(), v.end());
+
+  cout << "Contents of the sorted vector: " << endl;
+  display(v);
+
+  cout << "END\n";
+  return 0;
+}
